64f436c0650de1474a39c7ed2323aed0fb348aa4
Micah Wine <mercyfulfate@gmail.com>
2022-03-21 19:08:02 -0400

    updated planck keymappings

--- keyboards/planck/keymaps/rums/keymap.c
+++ keyboards/planck/keymaps/rums/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |      |   1  |   2  |   3  |   4  |   5  |   6  |   -  |   =  |   +  |   _  |  Esc |
+ * |   =  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  Esc |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Bksp |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | Left | Down |  Up  |Right |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    _______, KC_1, KC_2,  KC_3, KC_4, KC_5, KC_6, KC_MINUS, KC_EQUAL, KC_PLUS, KC_UNDS,  KC_ESC,
+    KC_EQUAL, KC_1, KC_2,  KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,  KC_ESC,
     KC_BSPC, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, KC_BSLS,
     _______, KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME,  KC_PGDN,  KC_PGUP, KC_END,   _______,
     KC_GRAVE, _______, _______, _______, _______, _______, _______, _______,  KC_MNXT,  KC_VOLD, KC_VOLU,  KC_MPLY
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   {  |   }  |   &  |      |   7  |   8  |   9  |      |  Esc |
+ * |   ~  |   !  |   @  |   {  |   }  |   &  |      |   7  |   8  |   9  |   _  |  Esc |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Bksp |   #  |   $  |   (  |   )  |   *  |      |   4  |   5  |   6  |   -  |   /  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_TILD,  KC_EXLM, KC_AT,     KC_LCBR, KC_RCBR, KC_AMPR, _______, KC_P7,   KC_P8,   KC_P9,   _______, KC_ESC,
+    KC_TILD,  KC_EXLM, KC_AT,     KC_LCBR, KC_RCBR, KC_AMPR, _______, KC_P7,   KC_P8,   KC_P9,   KC_UNDERSCORE, KC_ESC,
     KC_BSPC,  KC_HASH, KC_DOLLAR, KC_LPRN, KC_RPRN, KC_ASTR, _______, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, KC_PSLS,
     _______,  KC_PERC, KC_CIRC,   KC_LBRC, KC_RBRC, KC_PIPE, _______, KC_P1,   KC_P2,   KC_P3,   KC_PPLS, KC_PAST,
     KC_GRAVE, _______, _______,   _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_PENT, KC_MPLY
